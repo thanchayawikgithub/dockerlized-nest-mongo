fa35f58338036a5af1ce2e6744466dfcd693ab7f
thanchayawik <thanchayawik@gmail.com>
2024-07-28 19:09:44 +0700

    connect nest to mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://root:rootpassword@localhost:27017/mydb?authSource=admin',
+    ),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
